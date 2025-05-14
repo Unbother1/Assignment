9f95df467f2dcb5c5254db592bcbd9bff22a5ff1
Unbother1 <kandenuanuoluwapo@gmail.com>
2025-05-14 13:17:51 +0100

    Called API using the Post method

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,48 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { GrFormView } from "react-icons/gr";
 import { Link, Navigate, useNavigate } from "react-router-dom";
  
  const Login = () => {
-    const [email, setEmail] = useState("")
+    const [username, setUsername] = useState("")
 
     const [password, setPassword] = useState("")
 
     const [isChecked, setIsChecked] = useState(false)
 
     const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
 
-    const [successMessage, setSuccessMessage] = useState(false)
 
-
-    const [ShowPassword, setShowPassword] = useState(false);
-    const togglePassword = () => {
-        setShowPassword(! ShowPassword);
-    };
+   
     const Navigate = useNavigate()
 
 
-    const verify = (e) => {
-        if((email) && (password) && (isChecked)) {
-            Navigate("/homepage")
-        } else {
-            setError(true)
-            
-        }
+    const verify = async (e) => {
+    
+      try {
+        e.preventDefault()
 
-        // alert("Your email and password is correct " +Email, Password)
-    }
+        const response = await axios.post("https://dummyjson.com/auth/login", {
+        username,
+        password,
+      });
+      setTimeout(() => {
+        Navigate("/")
+        setLoading(false)
+      }, 5000);
+      setLoading(true);
+      console.log(response);
+
+      } catch(error) {
+        console.error("error", error);
+        setError(true)
+        setErrorMessage(error.response.data.message)
+
+      }    
+        }
+  
    return (
      <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex items-center justify-center">
        <div className="bg-white p-8 rounded-2xl shadow-2xl w-full max-w-md">
@@ -40,13 +52,13 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
            <form onSubmit={verify} className="space-y-4">
              <div>
                <label className="block text-sm font-medium text-gray-700">
-                 Email
+                 Username
                </label>
                <input
-                 type="email"
-                 value={email}
-                 onChange={(e) => setEmail(e.target.value)}
-                 placeholder="Enter your email"
+                 type="username"
+                 value={username}
+                 onChange={(e) => setUsername(e.target.value)}
+                 placeholder="Enter your username"
                  className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                />
              </div>
@@ -55,27 +67,47 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
                  Password
                </label>
                <input
-                 type={ShowPassword ? "text" : "password"}
+                 type="text"
                  value={password}
                  onChange={(e) => setPassword(e.target.value)}
                  placeholder="Enter your password"
                  className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                ></input>
-               <div className="flex gap-80 mx-0">
+               <div className="flex gap-80 mx-0 my-4">
                  <input type="checkbox" onChange={(e) => setIsChecked(e.target.value)}/>
-                 <p onClick={togglePassword} className="cursor-pointer "><GrFormView /></p>
                </div>
              </div>
            
-              { error &&
-                  <p className="text-red-500">Please fill in all credentials</p>}
-              { successMessage &&
-                  <p className="text-green-400">loggedin successfully</p>}
+               { error &&
+                  <p className="text-red-500">{errorMessage}</p>}
+              {/* { successMessage &&
+                  <p className="text-green-400">loggedin successfully</p>} */}
              <button
                type="submit"
-               className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-200"
+               className="w-full text-center flex justify-center bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-200"
              >
-               Sign In
+             { loading? 
+             (<svg
+               className="animate-spin h-8 w-8 text-blue-500"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24"
+             >
+               <circle
+                 className="opacity-25"
+                 cx="12"
+                 cy="12"
+                 r="10"
+                 stroke="currentColor"
+                 strokeWidth="4"
+               ></circle>
+               <path
+                 className="opacity-75"
+                 fill="currentColor"
+                 d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+               ></path>
+             </svg>) : (" Sign In")
+             }
              </button>
            </form>
          <div className="text-center mt-4 text-sm text-gray-600">
@@ -83,10 +115,8 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
              Forgot password?
            </a>{" "}
            |
-           <Link to= "/signup">
-             <a href="#" className="hover:underline ml-2">
+           <Link to= "/signup" className="hover:underline ml-2">
                Create an account
-             </a>
            </Link>
          </div>
        </div>
